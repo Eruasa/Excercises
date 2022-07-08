7fc566cc595f1508918f321164b684a58ac1d12f
Eruasa <t.kemmler@gmx.de>
2022-07-08 07:53:21 +0200

    Hi

--- ex_02_02.py
+++ ex_02_02.py
@@ -1,7 +1,2 @@
 nzt  = input('Enter your name: ')
 print ("Hello", nzt)
-feeling = input('How are you? ')
-
-if feeling == 'Good':
-    print('Sounds good!')
-else: print('Hope you get a better day!')
---
+++ ex_02_03.py
@@ -0,0 +1,4 @@
+xh = input("Enter Hours: ")
+xr = input("Enter Rate: ")
+xp = float(xh) * float(xr) #Use float(variable) to convert a variable into a float
+print("Pay:", xp)
